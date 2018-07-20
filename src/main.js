e5940fbba81b271cb96dc0cd70d9518493139edb
Thomas Kerbrat <kerbrat@outlook.com>
2018-07-21 01:13:28 +0200

    Process command line arguments before executing the program

--- src/main.js
+++ src/main.js
@@ -1,16 +1,67 @@
 
+const { help } = require('./utils/help.js');
+
+// Checking for arguments options
+
+if (process.argv.length < 3) {
+    help();
+    process.exit(1);
+}
+
+let argNoColors = false, argInline = false;
+for (let argument of process.argv) {
+    if (['-h', '--help'].includes(argument)) {
+        help();
+        process.exit(0);
+    }
+
+    if (['-n', '--no-colors'].includes(argument)) {
+        argNoColors = true;
+        continue;
+    }
+
+    if (['-i', '--inline'].includes(argument)) {
+        argInline = true;
+        continue;
+    }
+}
+
+// Requires
+
 const fs = require('fs');
 const os = require('os');
 
-const { MultiLineineVisitor } = require('./visitors/multi-line-visitor');
+const { InlineineVisitor } = require('./visitors/inline-visitor.js');
+const { MultiLineineVisitor } = require('./visitors/multi-line-visitor.js');
 const { Parser } = require('./parsing/parser.js');
 
-const [, , path] = process.argv;
-const stringContent = fs.readFileSync(path, 'utf-8');
-const content = JSON.parse(stringContent);
-const rootNode = Parser.parse(content);
+const path = process.argv[process.argv.length - 1];
 
+// Validate inputs
+
+let stringContent;
+try {
+    stringContent = fs.readFileSync(path, 'utf-8');
+} catch (error) {
+    console.log([path, ': No such file'].join(''));
+    process.exit(1);
+}
+
+let content;
+try {
+    content = JSON.parse(stringContent);
+} catch (error) {
+    console.log([path, ': ', error.message].join(''));
+    process.exit(1);
+}
+
+// json_ppc
+
+const rootNode = Parser.parse(content);
 const stringBuilder = [];
-const visitor = new MultiLineineVisitor(stringBuilder, os.EOL);
+const visitor = argInline
+    ? new InlineineVisitor(stringBuilder, !argNoColors)
+    : new MultiLineineVisitor(stringBuilder, os.EOL, !argNoColors);
+
 visitor.visit(rootNode);
 console.log(stringBuilder.join(''));
